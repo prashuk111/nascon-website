672fbd76fe57fe33bda1bca67bb5fc3a87ccd663
APPLE <ibm@ibms-MacBook-Pro.local>
2026-08-31 13:27:29 +0530

    style: remove connect with us social media section

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -178,55 +178,6 @@ function CTA() {
       </div>
 
       {/* =========================
-          SOCIAL MEDIA LINKS
-      ========================= */}
-      <div className="contact-social-links">
-        <span>Connect With Us:</span>
-        <div className="contact-social-icons">
-          <a
-            href="https://www.linkedin.com/company/nascon-technologies"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Nascon Technologies on LinkedIn"
-          >
-            LinkedIn
-          </a>
-          <a
-            href="https://twitter.com/nascontech"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Nascon Technologies on X"
-          >
-            X (Twitter)
-          </a>
-          <a
-            href="https://www.facebook.com/nascontechnologies"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Nascon Technologies on Facebook"
-          >
-            Facebook
-          </a>
-          <a
-            href="https://www.instagram.com/nascontechnologies"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Nascon Technologies on Instagram"
-          >
-            Instagram
-          </a>
-          <a
-            href="https://www.youtube.com/@nascontechnologies"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Nascon Technologies on YouTube"
-          >
-            YouTube
-          </a>
-        </div>
-      </div>
-
-      {/* =========================
           DEVELOPMENT MILESTONE
       ========================= */}
 
